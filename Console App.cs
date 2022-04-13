bfec78bf9e4c850fe1dd68cd9ceeb98f9f639dd5
JoshMayBalGithub <93077235+JoshMayBalGitHub@users.noreply.github.com>
2022-04-13 16:13:56 +0800

    updated to new version

--- Console App.cs
+++ Console App.cs
@@ -17,19 +17,102 @@ class Program {
     else if (ansactive == "N") {
         Console.WriteLine("Okay, canceling...");
         System.Threading.Thread.Sleep(TimeSpan.FromSeconds(3));
-        Environment.Exit(1);
+        Environment.Exit(0);
     } 
     else {
         Console.WriteLine("Woops, an ERROR, try again would ya?");
         System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
         goto Fb;
     }
-    Console.WriteLine("Do you want to install the Source Code?");
+
+     Console.WriteLine("Starting Installer......");
+     System.Threading.Thread.Sleep(TimeSpan.FromSeconds(5));
+     Console.WriteLine("Getting some files ready.....");
+     System.Threading.Thread.Sleep(TimeSpan.FromSeconds(5));
+     Console.WriteLine("Checking file integrity....");
+     System.Threading.Thread.Sleep(TimeSpan.FromSeconds(2));
+     Console.WriteLine("Installing.....");
+     System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+     Console.WriteLine("Installing textwars.csproj.....");
+     System.Threading.Thread.Sleep(TimeSpan.FromSeconds(5));
+     Console.WriteLine("Done!");
+     System.Threading.Thread.Sleep(TimeSpan.FromSeconds(2));
+     Console.WriteLine("Installing textwars.cs.....");
+     System.Threading.Thread.Sleep(TimeSpan.FromSeconds(5));
+     Console.WriteLine("Done!");
+     System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+     Console.WriteLine("Installing bin stuff.......");
+     System.Threading.Thread.Sleep(TimeSpan.FromSeconds(7));
+     Console.WriteLine("Done!");
+     System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+     Console.WriteLine("Installing obj stuff..........");
+     System.Threading.Thread.Sleep(TimeSpan.FromSeconds(7));
+     Console.WriteLine("Done!");
+     System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+
+    Fb2:
+    Console.WriteLine("Do you wish to install the .vscode stuff?");
     Console.WriteLine("Y for yes and N for no");
-    string ansactive2 = Console.ReadLine();
-    if (ansactive2 == "Y") {
+     string ans2active = Console.ReadLine();
+    if (ans2active == "Y") {
+        Console.WriteLine("Okay.....");
+        System.Threading.Thread.Sleep(TimeSpan.FromSeconds(2));
+    }
+    else if (ans2active == "N") {
+        Console.WriteLine("Okay......");
+        System.Threading.Thread.Sleep(TimeSpan.FromSeconds(2));
+        goto Fb3;
+    } 
+    else {
+        Console.WriteLine("Woops, an ERROR, try again would ya?");
+        System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+        goto Fb2;
+    }
+    Console.WriteLine("Installing .vscode stuff......");
+    System.Threading.Thread.Sleep(TimeSpan.FromSeconds(5));
+    Console.WriteLine("Done!");
+    System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+    Fb3:
+    Console.WriteLine("Do you wish to install:");
+    Console.WriteLine("L for Linux Version");
+    Console.WriteLine("M for MacOS Version");
+    Console.WriteLine("W for Windows Version");
+    string ans3active = Console.ReadLine();
+    if (ans3active == "L") {
+        Console.WriteLine("Okay.....");
+        System.Threading.Thread.Sleep(TimeSpan.FromSeconds(2));
+    }
+    else if (ans3active == "M") {
+        Console.WriteLine("Okay......");
+        System.Threading.Thread.Sleep(TimeSpan.FromSeconds(2));
         
     }
+     else if (ans3active == "W") {
+        Console.WriteLine("Okay......");
+        System.Threading.Thread.Sleep(TimeSpan.FromSeconds(2));
+        goto Fb4;
+    } 
+    else {
+        Console.WriteLine("Woops, an ERROR, try again would ya?");
+        System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+        goto Fb3;
+    }
+    Console.WriteLine("Installing Run.sh");
+    System.Threading.Thread.Sleep(TimeSpan.FromSeconds(5));
+    Console.WriteLine("Done!");
+    System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+    goto Fb5;
 
+    Fb4:
+    Console.WriteLine("Installing Run.bat");
+    System.Threading.Thread.Sleep(TimeSpan.FromSeconds(5));
+    Console.WriteLine("Done!");
+    System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+    Fb5:
+
+    Console.WriteLine("Install Finished!");
+    System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+    Console.WriteLine("Press Enter to exit....");
+    string ans4active = Console.ReadLine();
     }
 }
